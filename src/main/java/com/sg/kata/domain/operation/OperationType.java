202453442dcdbc540fe9228b5f2a13395f4f74a6
chniti <ibrahim.chniti.dev@gmail.com>
2022-03-28 23:31:55 +0200

    add fromString method to OperationType
    Throw OperationTypeDoesNotExistException when fromString method called with non valid operation type

--- src/main/java/com/sg/kata/domain/operation/OperationType.java
+++ src/main/java/com/sg/kata/domain/operation/OperationType.java
@@ -1,8 +1,21 @@
 package com.sg.kata.domain.operation;
 
+import com.sg.kata.exception.OperationTypeDoesNotExistException;
+
 /**
  * @author iBrahim chniti
  */
 public enum OperationType {
-    DEPOSIT, WITHDRAW
+    DEPOSIT, WITHDRAW;
+
+    public static OperationType fromString(String type) {
+        switch (type.toUpperCase()) {
+            case "DEPOSIT":
+                return DEPOSIT;
+            case "WITHDRAW":
+                return WITHDRAW;
+            default:
+                throw new OperationTypeDoesNotExistException(type);
+        }
+    }
 }
